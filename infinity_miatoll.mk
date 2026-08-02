b72b3a6ef810b5306089861f634c2fbfb99f05a3
一スAciss <orlannihboss@gmail.com>
2026-08-03 00:36:40 +0700

    Update infinity_miatoll.mk

--- infinity_miatoll.mk
+++ infinity_miatoll.mk
@@ -31,3 +31,8 @@ PRODUCT_BUILD_PROP_OVERRIDES += \
     BuildDesc="miatoll_global-user 12 SKQ1.211019.001 V14.0.3.0.SJZMIXM release-keys" \
     BuildFingerprint=Redmi/miatoll_global/miatoll:12/RKQ1.211019.001/V14.0.3.0.SJZMIXM:user/release-keys
 
+
+# Infinity Flags
+INFINITY_MAINTAINER := "Nezuko™"
+TARGET_HAS_UDFPS := false
+WITH_GAPPS := true
