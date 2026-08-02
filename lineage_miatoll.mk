fc4f61debba362d7ce08112a27dbd9a35e972c3e
一スAciss <orlannihboss@gmail.com>
2026-08-02 21:02:42 +0700

    Update and rename custom_miatoll.mk to lineage_miatoll.mk

--- lineage_miatoll.mk
+++ lineage_miatoll.mk
@@ -10,7 +10,7 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/non_ab_device.mk)
 
 # Inherit some common PixelOS stuff.
-$(call inherit-product, vendor/custom/config/common_full_phone.mk)
+$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
 
 # Inherit from miatoll device
 $(call inherit-product, device/xiaomi/miatoll/device.mk)
@@ -19,7 +19,7 @@ $(call inherit-product, device/xiaomi/miatoll/device.mk)
 TARGET_SCREEN_HEIGHT := 2400
 TARGET_SCREEN_WIDTH := 1080
 
-PRODUCT_NAME := custom_miatoll
+PRODUCT_NAME := lineage_miatoll
 PRODUCT_DEVICE := miatoll
 PRODUCT_MANUFACTURER := Xiaomi
 PRODUCT_BRAND := Redmi
@@ -30,3 +30,18 @@ PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
 PRODUCT_BUILD_PROP_OVERRIDES += \
     BuildDesc="miatoll_global-user 12 SKQ1.211019.001 V14.0.3.0.SJZMIXM release-keys" \
     BuildFingerprint=Redmi/miatoll_global/miatoll:12/RKQ1.211019.001/V14.0.3.0.SJZMIXM:user/release-keys
+
+
+# Axion flags
+# Camera information (multiple sensors supported)
+AXION_CAMERA_REAR_INFO := 64,8,5
+AXION_CAMERA_FRONT_INFO := 16
+TARGET_ENABLE_BLUR := true
+TARGET_SUPPORTED_REFRESH_RATES := 60
+TARGET_DOZE_DOUBLE_TAP_PULSE_SUPPORTED := true
+TARGET_INCLUDE_GOOGLE_TELECOMM := false
+# Maintainer name (underscores become spaces in the UI)
+AXION_MAINTAINER := Nezuko™
+TARGET_DISABLES_LIBPERF ?= true
+# Processor name (underscores become spaces)
+AXION_PROCESSOR := Snapdragon™ 720G
