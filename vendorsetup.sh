6d2ca0b6aecd11d6a5a5daca20b28d8899df3fda
一スAciss <orlannihboss@gmail.com>
2026-08-02 21:06:22 +0700

    Update vendorsetup.sh

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -9,7 +9,10 @@ git clone https://github.com/kardebayan/android_device_xiaomi_miuicamera-miatoll
 git clone https://github.com/kardebayan/android_vendor_xiaomi_miuicamera-miatoll.git -b 16.0-miatoll vendor/xiaomi/miuicamera-miatoll
 
 # Kernel
-git clone https://github.com/Michikoextv2/kernel_xiaomi_sm6250.git -b main kernel/xiaomi/sm6250
+git clone https://github.com/Michikoextv2/kernel_xiaomi_sm6250.git -b main kernel/xiaomi/sm6250 --depth 1
 
 # Dolby
 git clone https://github.com/yaap/vendor_sony_dolby.git -b sixteen vendor/sony/dolby
+
+# Time
+git clone https://github.com/LineageOS/android_hardware_sony_timekeep.git -b lineage-23.2 hardware/sony/timekeep
