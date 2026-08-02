670d1e90a36b84d64d78d29f67ae04b1fceca528
一スAciss <orlannihboss@gmail.com>
2026-08-02 20:57:33 +0700

    Create vendorsetup.sh

---
+++ vendorsetup.sh
@@ -0,0 +1,15 @@
+# device 
+git clone https://github.com/Shadowwisperia/android_vendor_xiaomi_miatoll.git -b main vendor/xiaomi/miatoll
+
+# Hardware
+git clone https://github.com/LineageOS/android_hardware_xiaomi.git -b lineage-23.2 hardware/xiaomi
+
+# Miui cam 
+git clone https://github.com/kardebayan/android_device_xiaomi_miuicamera-miatoll.git -b 16.0-miatoll device/xiaomi/miuicamera-miatoll
+git clone https://github.com/kardebayan/android_vendor_xiaomi_miuicamera-miatoll.git -b 16.0-miatoll vendor/xiaomi/miuicamera-miatoll
+
+# Kernel
+git clone https://github.com/Michikoextv2/kernel_xiaomi_sm6250.git -b main kernel/xiaomi/sm6250
+
+# Dolby
+git clone https://github.com/yaap/vendor_sony_dolby.git -b sixteen vendor/sony/dolby
